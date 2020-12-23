92a55398abb144a5391eb470d2add87f0d49ea7b
李世星 <70449@any3.com>
2020-12-23 17:17:56 +0800

    ubo

--- app/src/main/java/com/xingfeng/opengles/chapter20/Chapter20Activity.java
+++ app/src/main/java/com/xingfeng/opengles/chapter20/Chapter20Activity.java
@@ -5,18 +5,21 @@ import android.os.Bundle;
 import com.xingfeng.opengles.BaseListActivity;
 import com.xingfeng.opengles.chapter20.chapter201.Chapter201Activity;
 import com.xingfeng.opengles.chapter20.chapter202.Chapter202Activity;
+import com.xingfeng.opengles.chapter20.chapter203.Chapter203Activity;
 
 public class Chapter20Activity extends BaseListActivity {
 
     private Class[] classes = new Class[] {
             Chapter201Activity.class,
-            Chapter202Activity.class
+            Chapter202Activity.class,
+            Chapter203Activity.class
     };
 
 
     private String[] classDescription = new String[] {
             "缓冲区对象-顶点缓冲区-VBO",
-            "缓冲区对象-顶点数组缓冲区-VAO"
+            "缓冲区对象-顶点数组缓冲区-VAO",
+            "缓冲区对象-一致缓冲区对象-UBO"
     };
 
     @Override
